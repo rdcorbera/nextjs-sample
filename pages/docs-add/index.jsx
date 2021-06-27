3ac4b7fd6c201ee6c3de2e0d4f7c68925c321e96
Ricardo Corbera <rdcorbera@gmail.com>
2021-06-27 19:29:41 -0400

    Added Button component

--- pages/docs-add/index.jsx
+++ pages/docs-add/index.jsx
@@ -1,7 +1,11 @@
-import Head from 'next/head'
+import styles from './index.module.css'
+import Button from '../../src/core/components/atoms/button'
 
 export default function DocsAdd() {
   return (
-    <div>Add travel documentation</div>
+    <div>
+      <h1>Add travel documentation</h1>
+      <Button text="Save" />
+    </div>
   )
 }
