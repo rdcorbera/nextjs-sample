66c50a75386b1ec6d0cc10819fc1fb74e5627460
Ricardo Corbera <rdcorbera@gmail.com>
2021-07-02 00:16:30 -0400

    Handle 404

--- pages/reservation/[id].jsx
+++ pages/reservation/[id].jsx
@@ -3,6 +3,13 @@ import ReservationModule from '../../modules/reservation'
 export async function getServerSideProps(context) {
   const { id } = context.params
   const res = await fetch(`http://localhost:3100/api/reservations/${id}`)
+
+  if (res.status == 404) {
+    return {
+      notFound: true,
+    }
+  }
+
   const reservation = await res.json()
 
   // Pass data to the page via props
